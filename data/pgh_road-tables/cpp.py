b570ffa9c17e482d6e57451c4acee6d5f2fd889e
Scott Weingart <weingart.scott@gmail.com>
2018-06-21 19:56:06 -0400

    cpp
    cpp is an implementation of the chinese postman problem that finds the most efficient route through the bridges

---
+++ data/pgh_road-tables/cpp.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 21 19:17:20 2018
+
+@author: weing
+"""
+
+import postman_problems
+
+# find CPP solution
+circuitSmall, graphSmall = cpp(edgelist_filename='component-connections-edgelist-reduced-degree-2.csv', start_node='1')
+circuit, graph = cpp(edgelist_filename='component-connections-edgelist.csv', start_node='1')
+
+
+# print solution route
+for e in circuit:
+    print(e)
+
+# print solution route
+for e in circuitSmall:
+    print(e)
+
+
+# print solution summary stats
+for k, v in calculate_postman_solution_stats(circuitSmall).items():
+    print(k, v)
+    
+for k, v in calculate_postman_solution_stats(circuit).items():
+    print(k, v)
