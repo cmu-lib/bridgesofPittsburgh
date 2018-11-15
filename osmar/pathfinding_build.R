317e2f1bf7a82594af9fdd3146c044b6251f5837
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-11-15 11:40:47 -0500

    experiment with a dual cache for buildling graphs and then pathways

--- osmar/pathfinding_build.R
+++ osmar/pathfinding_build.R
@@ -1,31 +1,37 @@
 source("osmar/osmar_graph.R")
+graph_cache <- new_cache(".graph")
+make(pgh_plan, c("tiny_interface_points", "pgh_interface_points"), cache = graph_cache)
+orig_tiny_interface_points = readd("tiny_interface_points", cache = graph_cache)
+orig_pgh_interface_points = readd("pgh_interface_points", cache = graph_cache)
 
-make(pgh_plan, c("tiny_interface_points", "pgh_interface_points"))
-loadd(list = c("tidy_tiny_graph", "tiny_interface_points", "pgh_tidy_graph", "pgh_interface_points"), verbose = 4)
+path_cache <- new_cache(".path")
+original_targets <- drake_plan(
+  tidy_tiny_graph = readd("tidy_tiny_graph", cache = graph_cache),
+  pgh_tidy_graph = readd("pgh_tidy_graph", cache = graph_cache)
+)
 
-tiny_pathway_plan_generic <- drake_plan(tiny_pathway = greedy_search(starting_point = sp__, graph = tidy_tiny_graph))
-pgh_pathway_plan_generic <- drake_plan(pgh_pathway = greedy_search(starting_point = sp__, graph = pgh_tidy_graph))
+tiny_pathway_plan_generic <- drake_plan(tiny_pathway = greedy_search(starting_point = sp__, graph = tidy_tiny_graph, quiet = TRUE))
+pgh_pathway_plan_generic <- drake_plan(pgh_pathway = greedy_search(starting_point = sp__, graph = pgh_tidy_graph, quiet = TRUE))
 
-tiny_expanded_pathways <- evaluate_plan(tiny_pathway_plan_generic, rules = list(sp__ = tiny_interface_points))
-pgh_expanded_pathways <- evaluate_plan(pgh_pathway_plan_generic, rules = list(sp__ = pgh_interface_points))
+tiny_expanded_pathways <- evaluate_plan(tiny_pathway_plan_generic, rules = list(sp__ = orig_tiny_interface_points))
+pgh_expanded_pathways <- evaluate_plan(pgh_pathway_plan_generic, rules = list(sp__ = orig_pgh_interface_points))
 
 assessment_plan <- drake_plan(
-  tiny_results = map(set_names(tiny_expanded_pathways$target), readd, character_only = TRUE),
+  tiny_results = map(set_names(tiny_expanded_pathways$target), readd, character_only = TRUE, cache = path_cache),
   tiny_performances = map_df(tiny_results, function(x) data_frame(point = x$pathfinding_results$point, distance = x$path_distance, missing_points = length(x$pathfinding_results$search_set)), .id = "target") %>% 
     arrange(missing_points, distance) %>% 
     mutate(performance_rank = row_number()),
-  pgh_results = map(set_names(pgh_expanded_pathways$target), readd, character_only = TRUE),
+  pgh_results = map(set_names(pgh_expanded_pathways$target), readd, character_only = TRUE, cache = path_cache),
   pgh_performances = map_df(pgh_results, function(x) data_frame(point = x$pathfinding_results$point, distance = x$path_distance, missing_points = length(x$pathfinding_results$search_set)), .id = "target") %>% 
     arrange(missing_points, distance) %>% 
     mutate(performance_rank = row_number())
 )
 
 all_pathways <- bind_plans(
+  original_targets,
   tiny_expanded_pathways,
   pgh_expanded_pathways,
-  gather_plan(tiny_expanded_pathways, target = "tiny_pathways"),
-  gather_plan(pgh_expanded_pathways, target = "pgh_pathways"),
   assessment_plan
 )
 
-make(all_pathways, "pgh_pathways", jobs = 12)
+make(all_pathways, jobs = 12, cache = path_cache)
