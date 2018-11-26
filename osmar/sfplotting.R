7036d5554ca9b191761c916500383631800ddfb1
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-11-26 16:18:01 -0500

    refactor map plotting functions
    closes #70

--- osmar/sfplotting.R
+++ osmar/sfplotting.R
@@ -1,71 +1,96 @@
 library(sf)
-source("osmar/osmar_graph.R")
-source("osmar/pathfinding_build.R")
-loadd(c("tidy_tiny_graph", "pgh_tidy_graph"), cache = graph_cache)
 library(fs)
+source("osmar/pathfinding_build.R")
+loadd(c("tidy_tiny_graph", "pgh_tidy_graph"), cache = path_cache)
+loadd("pgh_pathway_2489", cache = path_cache)
 
-#res1027 <- greedy_search(1027, pgh_tidy_graph)
-load("res1027.rda")
+mark_graph <- function(graph, pathway) {
+  path_ids <- unique(flatten_int(pathway$epath))
+  
+  marked_graph <- graph %>% 
+    activate(edges) %>% 
+    mutate(
+      edge_order = match(row_number(), path_ids),
+      flagged_edge = !is.na(edge_order),
+      edge_label = if_else(is.na(edge_order), "", as.character(edge_order)),
+      edge_category = factor(case_when(
+        is_bridge & flagged_edge ~ "crossed bridge",
+        !is_bridge & flagged_edge ~ "crossed road",
+        is_bridge & !flagged_edge ~ "uncrossed bridge",
+        TRUE ~ "uncrossed road"
+      ), levels = c("crossed bridge", "crossed road", "uncrossed bridge", "uncrossed road"),
+      ordered = TRUE)
+    ) %>% 
+    filter(within_boundaries | flagged_edge) %>% 
+    select_main_component() %>% 
+    # Simplify multiple edges on all two-way roads so that we only plot one line
+    # per road. The edge category is a ranked factor, so it will plot as a
+    # crossed bridge before being plotted as anything else, etc.
+    simplify(remove.multiple = TRUE, edge.attr.comb = list(edge_cateogry = "max", flagged_edge = "max", "first")) %>% 
+    as_tbl_graph() %>% 
+    activate(edges) %>% 
+    mutate_at(vars(flagged_edge), as.factor(as.logical(.)))
+  
+  marked_graph
+}
 
-path_ids <- res2489$epath %>% 
-  flatten_int()
+flagged_graph_as_sf <- function(marked_graph) {
+  edges <- as_tibble(marked_graph, "edges") %>% arrange(.id)
+  nodes <- as_tibble(marked_graph, "nodes") %>% mutate(index = row_number())
+  
+  st_edges <- edges %>% 
+    select(.id, from, to) %>% 
+    gather(key = "dim", value = "index", from, to) %>%
+    arrange(.id, dim) %>% 
+    left_join(select(nodes, lat, lon, index), by = "index") %>% 
+    split(as.factor(.$.id)) %>% 
+    map(function(x) {
+      cbind(x$lon, x$lat)
+    }) %>% 
+    map(st_linestring)
+  
+  sf_col <- st_sfc(st_edges, crs = 4326)
+  st_geometry(edges) <- sf_col
+  edges
+}
 
-graph <- pgh_tidy_graph %>% 
-  activate(edges) %>% 
-  mutate(
-    edge_order = match(row_number(), path_ids),
-    flagged_edge = !is.na(edge_order),
-    edge_label = if_else(is.na(edge_order), "", as.character(edge_order)),
-    edge_category = as.factor(case_when(
-      is_bridge & flagged_edge ~ "crossed bridge",
-      !is_bridge & flagged_edge ~ "crossed road",
-      is_bridge & !flagged_edge ~ "uncrossed bridge",
-      TRUE ~ "uncrossed road"
-    ))
-  ) %>% 
-  filter(within_boundaries | flagged_edge) %>% 
-  select_main_component()
+graph_to_sf <- function(graph, pathway) {
+  flagged_graph <- mark_graph(graph, pathway)
+  flagged_graph_as_sf(flagged_graph)
+}
 
-nodes <- as_tibble(graph, "nodes") %>% 
-  mutate(index = row_number())
-edges <- as_tibble(graph, "edges")
-st_nodes <- st_multipoint(cbind(nodes$lon, nodes$lat), dim = "XY")
-st_edges <- edges %>% 
-  select(.id, from, to) %>% 
-  gather(key = "dim", value = "index", from, to) %>%
-  arrange(.id, dim) %>% 
-  left_join(select(nodes, lat, lon, index), by = "index") %>% 
-  split(as.factor(.$.id)) %>% 
-  map(function(x) {
-    cbind(x$lon, x$lat)
-  }) %>% 
-  map(st_linestring)
+# Plotting ----
 
-pgh_sf <- st_sfc(st_edges, crs = 4326)
+bridges_only_plot <- function(edges, filepath) {
+  pdf(filepath, width = 20, height = 20)
+  plot(edges["is_bridge"], pal = c(
+    "TRUE" = "gray",
+    "FALSE" = "red"
+  ), lwd = if_else(edges$is_bridge, 2, 0.5))
+  dev.off()
+}
 
-st_geometry(edges) <- pgh_sf
+pathway_plot <- function(edges, filepath) {
+  pdf(filepath, width = 20, height = 20)
+  plot(edges["edge_category"], pal = c(
+    "crossed bridge" = "#d95f02",
+    "crossed road" = "#1b9e77",
+    "uncrossed bridge" = "#e6ab02",
+    "uncrossed road" = "gray"
+  ), lwd = if_else(edges$flagged_edge, 2, 0.5))
+  dev.off()
+}
 
-
-
-
-pdf("big.pdf", width = 20, height = 20)
-plot(edges["edge_category"], pal = c(
-  "crossed bridge" = "#d95f02",
-  "crossed road" = "#1b9e77",
-  "uncrossed bridge" = "#e6ab02",
-  "uncrossed road" = "gray"
-), lwd = if_else(edges$flagged_edge, 2, 0.5))
-dev.off()
-
-oneplot <- function(e, i, edges) {
-  message(i)
+animation_frame <- function(e, i, edges, dirpath) {
   edges$highlight <- edges$.id %in% e
-  png(fs::path("maps", paste0(str_pad(i, width = 3, pad = "0", side = "left"), "map"), ext = "png"))
+  png(fs::path(dirpath, paste0(str_pad(i, width = 3, pad = "0", side = "left"), "map"), ext = "png"), height = 768, width = 1024)
   plot(edges["highlight"], pal = c("gray", "red"), lwd = if_else(edges$highlight, 3, 1))
   dev.off()
 }
 
-
-iwalk(res2489$epath, oneplot, edges = edges)
-
-plot(edges["geometry"])
+animation_plot <- function(pathway, edges, dirpath) {
+  # Keep only those pathway steps where at least one edge is crossed
+  non_empty_path <- discard(pathway$epath, ~ length(.) == 0)
+  fs::dir_create(dirpath)
+  iwalk(non_empty_path, animation_frame, edges = edges, dirpath = dirpath)
+}
